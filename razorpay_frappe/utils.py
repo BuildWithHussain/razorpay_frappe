fcaf1874e622c07223edf2a3da123d6bcd5e95e5
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-05-08 23:04:27 +0530

    feat: core razorpay utils

---
+++ razorpay_frappe/utils.py
@@ -0,0 +1,28 @@
+import frappe
+import razorpay
+from frappe.utils.password import get_decrypted_password
+
+
+def get_razorpay_client():
+	razorpay_settings = frappe.get_cached_doc("Razorpay Settings")
+	key_id = razorpay_settings.key_id
+	key_secret = razorpay_settings.get_password("key_secret")
+
+	if not (key_id or key_secret):
+		frappe.throw(
+			f"Please set API keys in {frappe.bold('Razorpay Settings')} before trying to create a razorpay client!"
+		)
+
+	return razorpay.Client(auth=(key_id, key_secret))
+
+
+def get_in_razorpay_money(amount: int) -> int:
+	return amount * 100
+
+
+def verify_webhook_signature(payload):
+	signature = frappe.get_request_header("X-Razorpay-Signature")
+	webhook_secret = get_decrypted_password("Razorpay Settings", "Razorpay Settings", "webhook_secret")
+
+	client = get_razorpay_client()
+	client.utility.verify_webhook_signature(payload.decode(), signature, webhook_secret)
