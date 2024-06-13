5502f60008d480ad2b93a3c4447941e2ca2a9764
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-06-14 00:02:39 +0530

    fix: don't show fetch status in new form

--- razorpay_frappe/razorpay_integration/doctype/razorpay_payment_link/razorpay_payment_link.js
+++ razorpay_frappe/razorpay_integration/doctype/razorpay_payment_link/razorpay_payment_link.js
@@ -10,7 +10,7 @@ frappe.ui.form.on("Razorpay Payment Link", {
 			});
 		}
 
-		if (frm.doc.status !== "Expired") {
+		if (frm.doc.status !== "Expired" && !frm.is_new()) {
 			frm.add_custom_button("Fetch Status", () => {
 				frm.call("fetch_latest_status").then(() => {
 					frappe.show_alert("Latest status updated");
