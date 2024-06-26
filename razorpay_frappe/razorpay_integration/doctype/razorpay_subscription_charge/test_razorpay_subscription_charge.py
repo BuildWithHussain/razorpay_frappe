b39a620e61570b7d3ba94215790c55a14be5a80e
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-06-26 21:19:17 +0530

    chore: delete charge doctype, no longer needed

--- razorpay_frappe/razorpay_integration/doctype/razorpay_subscription_charge/test_razorpay_subscription_charge.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Build With Hussain and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestRazorpaySubscriptionCharge(FrappeTestCase):
-	pass
