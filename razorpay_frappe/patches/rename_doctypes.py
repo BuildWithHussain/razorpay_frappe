21dd4dbcc0b48ad90aea7f5bc8a2ddc6c2bab72a
vishwajeet-13 <vishwajeetthakur403@gmail.com>
2026-01-03 16:59:15 +0530

    chore: remove sequence

--- razorpay_frappe/patches/rename_doctypes.py
+++ razorpay_frappe/patches/rename_doctypes.py
@@ -7,7 +7,6 @@ doctypes = {
 
 def execute():
 	rename_doctypes()
-	create_sequences()
 
 
 def rename_doctypes():
@@ -18,15 +17,3 @@ def rename_doctypes():
 			frappe.rename_doc(
 				"DocType", old, new, force=True, ignore_if_exists=True
 			)
-
-
-def create_sequences():
-	doctype = "Frappe Razorpay Settings"
-	sequence_name = frappe.scrub(doctype + "_id_seq")
-	frappe.db.sql_ddl(f"drop sequence if exists {sequence_name}")
-	last_name = frappe.db.get_all(doctype, fields=["max(name) as last"])[0].last
-	start_value = (last_name or 0) + 1
-	print(f"Creating sequence for {doctype} starting at {start_value}")
-	frappe.db.create_sequence(
-		doctype, start_value=start_value, check_not_exists=True
-	)
