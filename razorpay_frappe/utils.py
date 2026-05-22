df59106006853f35b856ded308048550dcf16648
GowthamL <45562756+Gowtham-L@users.noreply.github.com>
2026-05-22 15:42:17 +0530

    fix: setting from custom app

--- razorpay_frappe/utils.py
+++ razorpay_frappe/utils.py
@@ -24,21 +24,41 @@ class RazorpaySubscriptionWebhookEvents(StrEnum):
 
 
 def get_razorpay_client():
-	in_ci = os.environ.get("CI")
-	if not in_ci:
-		razorpay_settings = frappe.get_cached_doc("Razorpay Settings")
-		key_id = razorpay_settings.key_id
-		key_secret = razorpay_settings.get_password("key_secret")
-	else:
-		key_id = os.environ.get("RZP_SANDBOX_KEY_ID")
-		key_secret = os.environ.get("RZP_SANDBOX_KEY_SECRET")
-
-	if not (key_id or key_secret):
-		frappe.throw(
-			f"Please set API keys in {frappe.bold('Razorpay Settings')} before trying to create a razorpay client!"
-		)
-
-	return razorpay.Client(auth=(key_id, key_secret))
+    in_ci = os.environ.get("CI")
+    if not in_ci:
+        razorpay_settings = frappe.get_cached_doc(
+            "Razorpay Settings"
+        )
+        key_id = getattr(
+            razorpay_settings,
+            "api_key",
+            None
+        ) or getattr(
+            razorpay_settings,
+            "key_id",
+            None
+        )
+        key_secret = razorpay_settings.get_password(
+            "api_secret"
+        ) or razorpay_settings.get_password(
+            "key_secret"
+        )
+    else:
+        key_id = os.environ.get(
+            "RZP_SANDBOX_KEY_ID"
+        )
+        key_secret = os.environ.get(
+            "RZP_SANDBOX_KEY_SECRET"
+        )
+    if not (key_id and key_secret):
+        frappe.throw(
+            f"Please set API keys in "
+            f"{frappe.bold('Razorpay Settings')} "
+            f"before trying to create a razorpay client!"
+        )
+    return razorpay.Client(
+        auth=(key_id, key_secret)
+    )
 
 
 def get_in_razorpay_money(amount: int) -> int:
